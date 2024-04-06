9f2924403fc68b5544456c931ddbfdbc34b981b4
DucTuan <tuanhypc02@gmail.com>
2024-04-06 10:47:25 +0700

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by macOS on 06/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
